519fb5dd1d69f303c8a9def6fe9ef89ef4b19dea
Zedmous <ejnsilva29@gmail.com>
2024-06-04 05:50:11 -0400

    comentarios de las salidas

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Bienvenidos a los juegos")
+
+
+/**
+ *  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+// imprime las salidas correspondientes 
+// usando las funciones ya codificadas en las otras ramas
+    //suma 7+8
+       
+    //resta 15-20
+
+    //multiplica -5 * -100
+
+    //divide el resultado de la multiplicacion entre 50
+// 
+
