9c7b18396ad939d809faab6b680097a61d1620f0
Zedmous <ejnsilva29@gmail.com>
2024-06-04 05:26:55 -0400

    rama b

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Bienvenidos a los juegos")
+
+
+
+
+
+multiplicacion =(a,b)=>{
+    return a*b
+}
+
+
---
+++ clases/calculadora.js
@@ -0,0 +1,18 @@
+
+
+
+class Calculadora{
+
+
+    constructor(){
+
+    }
+
+    division =(a,b)=>{
+        
+        if(b!=0){
+            return a/b
+        }
+        return 0
+    }
+}
